5e93081649a8822e20b8286585e459377eb3fa5f
Kobus <kobus@infinityarc.net>
2021-07-27 11:39:09 +0200

    completed creatue a feature

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero'
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Zef' },
+  { id: 12, name: 'Captain Mountain Dew' },
+  { id: 13, name: 'Dr Docx' },
+  { id: 14, name: 'MassiveClint' },
+  { id: 15, name: 'Mr Angular' },
+  { id: 16, name: 'Pickle RIck' },
+  { id: 17, name: 'Jetman JPEG' },
+  { id: 18, name: 'Backpack man' },
+  { id: 19, name: 'Poke-a-coconut guy' },
+  { id: 20, name: 'See Eend Brah' }
 ];
